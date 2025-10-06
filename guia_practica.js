7d1050921879fd6632ef4f271b806f98fb5b86b4
YkerMendoza <nolsfireandblood@gmail.com>
2025-10-05 20:27:00 -0500

    Add files via upload

---
+++ guia_practica.js
@@ -0,0 +1,11 @@
+/*Contar caracteres de una palabra
+o Entrada: "Manzana"
+o Salida: "La palabra tiene 7 letras."
+*/
+function ContarPalabras(){
+    palabra= prompt("Inserte la palabra a contar:")
+    contador= palabra.length 
+    console.log (`La palabra ${palabra} tiene ${contador} letras`)
+}
+ContarPalabras()
+/**/ 
